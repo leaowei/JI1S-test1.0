40ac2db2cb8a79c12ea750db4c9d50d64baa13f8
wei <41771631@qq.com>
2021-03-11 18:47:59 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+
+# Modify hostname
+sed -i 's/OpenWrt/OpenWrt-Ji1S/g' package/base-files/files/bin/config_generate
